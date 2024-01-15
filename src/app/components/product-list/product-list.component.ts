3ef65b179fde9b7cc4027c3cd0c495b178a850a4
Rob Roselius <roselius@skilldistillery.com>
2024-01-15 12:01:41 -0700

    Complete directivesAndBindings drills

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+
+  products: Product[] = [
+    new Product(
+      'Oh, The Places You\'ll Go!',
+      11.39,
+      'https://images-na.ssl-images-amazon.com/images/I/518eq5NjZkL._SY160_.jpg',
+      5
+    ),
+    new Product(
+      'The Giving Tree',
+      8.31,
+      'https://images-na.ssl-images-amazon.com/images/I/51XKHozHPEL._SY160_.jpg',
+      4
+    ),
+    new Product(
+      'Pete the Cat: Big Easter Adventure',
+      7.29,
+      'https://images-na.ssl-images-amazon.com/images/I/61VqwEXgCdL._AC_US218_.jpg',
+      4
+    ),
+    new Product(
+      'Harry Potter and the Chamber of Secrets',
+      8.99,
+      'https://images-na.ssl-images-amazon.com/images/I/51PFoq3WlaL._AC_US218_.jpg',
+      5
+    )
+  ];
+
+  display = true;
+  clickCount = 0;
+
+  getNumberOfProducts(): number {
+    return this.products.length;
+  }
+
+  log(msg: any): void {
+    console.log(this.clickCount++ + ' ' + msg);
+  }
 
 }
